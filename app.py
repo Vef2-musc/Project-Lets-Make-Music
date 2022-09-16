e6efca52f264b9b97c4027a13e9d9eae40d414e5
fannarfent <fannarorn17@gmail.com>
2022-09-16 11:50:05 +0000

    lagaði meira með redirects

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template,redirect
 
 app=Flask(__name__,template_folder='templates')
 @app.route("/")
@@ -15,8 +15,8 @@ def login():
 def search():
     return render_template("search.html")
 @app.route("/back")
-def search():
-    return redirect("/", code=302)
+def back():
+    return redirect("/")
 
 if __name__ == "__main__":
     app.run(debug=True)

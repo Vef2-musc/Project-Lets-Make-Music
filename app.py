3d7b0579ff3d5d1d774f48ca22b04b2f208d027b
fannarfent <fannarorn17@gmail.com>
2022-09-16 11:20:10 +0000

    henti inn þannig við getum farið á allar síðurnar

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
-from flask import Flask
+import os
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app=Flask(__name__,template_folder='templates')
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return render_template("hompage.html")
+@app.route("/forsida")
+def forsida():
+    return render_template("index.html")
+@app.route("/login")
+def login():
+    return render_template("login.html")
+@app.route("/leit")
+def search():
+    return render_template("search.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)

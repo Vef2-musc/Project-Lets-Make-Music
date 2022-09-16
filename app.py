c716123270e7091ceee7e5695dc50e4fcc5c5738
agustbirgir <54802234+agustbirgir@users.noreply.github.com>
2022-09-16 12:34:03 +0000

    add pyrebase(NOT FINISHED)

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+from distutils.command.config import config
+import email
+import pyrebase
 import os
 from flask import Flask, render_template,redirect
 
@@ -20,6 +23,25 @@ def search():
 @app.route("/back")
 def back():
     return redirect("/")
+
+config = {
+    'apiKey': "AIzaSyDx-6TLZW_nTueInaxRaLTesOaiYW1GtJU",
+    'authDomain': "lets-make-music.firebaseapp.com",
+    'projectId': "lets-make-music",
+    'storageBucket': "lets-make-music.appspot.com",
+    'messagingSenderId': "153066543311",
+    'appId': "1:153066543311:web:2efa520260bb9f2316ffba",
+    'measurementId': "G-3SK39RETG9",
+    'databaseURL':""
+}
+firebase = pyrebase.initialize_app(config)
+auth = firebase.auth()
+
+email = 'test@gmail.com'
+password = '12345g'
+
+user = auth.create_user_with_email_and_password(email,password)
+print(user)
 
 if __name__ == "__main__":
     app.run(debug=True)

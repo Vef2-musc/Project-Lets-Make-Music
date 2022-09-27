452494faa0a05247babfe0f59134c57d0214d9d7
Saesi <saebjornga20@tskoli.is>
2022-09-27 11:22:39 +0000

    ok I pull up

--- app.py
+++ app.py
@@ -52,7 +52,6 @@ def forsida():
     return render_template("index.html")
 @app.route("/login", methods=['GET','POST'])
 def login():
-    
     return render_template("login.html")
 @app.route('/loggedin')
 def loggedin():

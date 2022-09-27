d23f6983a2d3e24accc229ed97277cf58071efa4
Saesi <saebjornga20@tskoli.is>
2022-09-27 12:51:51 +0000

    accepted

--- app.py
+++ app.py
@@ -54,18 +54,10 @@ def forsida():
 @app.route("/home")
 def home():
     return render_template("acthomepage.html")
-<<<<<<< HEAD
 '''@app.route("/login", methods=['GET','POST'])
 def login():
     return render_template("login.html")'''
 '''@app.route('/loggedin')
-=======
-#@app.route("/login", methods=['GET','POST'])
-#def login():
-    
-    #return render_template("login.html")
-@app.route('/loggedin')
->>>>>>> 092ce2280bbe80f23706d862c3d264c5afe33af8
 def loggedin():
     return render_template("acthomepage.html")'''
 
